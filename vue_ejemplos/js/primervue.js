82dd853375cd261c14114856b6c825af783c797b
martileos <martinevarez@gmail.com>
2019-05-07 12:06:51 -0600

    Primer ejemplo VUE.js

---
+++ vue_ejemplos/js/primervue.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+	el: "#app",
+	data: {
+		titulo: 'Hola mundo con VUE',
+		frutas: ['manzanas','peras','platano','mango'],
+		frutas2: [
+			{nombre:'manzana', cantidad: 10},
+			{nombre:'peras', cantidad: 0},
+			{nombre:'platano', cantidad: 20},
+			{nombre:'mango', cantidad: 80}
+		]
+	}
+});
