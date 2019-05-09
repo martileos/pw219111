52bc242caa4a2e7efde1a2182528c036d87c1dfb
martileos <martinevarez@gmail.com>
2019-05-09 11:55:49 -0600

    Frutas2 Vue.js

--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -1,13 +1,62 @@
 const app = new Vue({
-	el: "#app",
+	el: '#app',
 	data: {
-		titulo: 'Hola mundo con VUE',
-		frutas: ['manzanas','peras','platano','mango'],
+		titulo: 'Hola mundo pero con Vue',
+		frutas: ['Manzana','Uva','Peras','Fresas'],
 		frutas2: [
-			{nombre:'manzana', cantidad: 10},
-			{nombre:'peras', cantidad: 0},
-			{nombre:'platano', cantidad: 20},
-			{nombre:'mango', cantidad: 80}
-		]
+			{nombre:'Manzana',cantidad: 11},
+			{nombre:'Uva',cantidad: 0},
+			{nombre:'Peras',cantidad: 80},
+			{nombre:'Fresas',cantidad: 110}
+		],
+		nuevaFruta: '',
+		total: 0
+	},
+	methods: {
+		agregarFruta(){
+			//console.log("diste clic");
+			this.frutas.push(this.nuevaFruta);
+			this.frutas2.push({
+				nombre: this.nuevaFruta,
+				cantidad: 0
+			});
+			this.nuevaFruta = '';
+		}
+	},
+	computed: {
+		sumarFrutas(){
+			this.total = 0; 
+			for(fruta of this.frutas2){
+				this.total = this.total+fruta.cantidad;
+			}
+			return this.total;
+		}
 	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
